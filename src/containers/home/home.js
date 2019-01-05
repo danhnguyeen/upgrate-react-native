c95f90af20547c4b4d7b670d6822c460d179c7ba
Danh Nguyen <danhnguyen@Danhs-MacBook-Pro.local>
2019-01-05 14:12:57 +0700

    [Account Kit] added to iOS

--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button } from 'react-native';
+import RNAccountKit from 'react-native-facebook-account-kit'
 
 class Home extends Component {
+  componentDidMount() {
+    this.configureAccountKit();
+  }
+  loginWithPhone = () => {
+    RNAccountKit.loginWithPhone()
+      .then((token) => {
+        console.log(token)
+        if (!token) {
+          console.log('Login cancelled')
+        } else {
+          console.log(`Logged with phone. Token: ${token}`)
+        }
+      })
+  }
+  configureAccountKit() {
+    RNAccountKit.configure({
+      // responseType: 'code',
+      initialPhoneCountryPrefix: '+84', // autodetected if none is provided
+      // viewControllerMode: 'show'|'present' // for iOS only, 'present' by default
+    })
+  }
   render() {
     return (
       <View>
         <Text>Home</Text>
+        <Button onPress={this.loginWithPhone} title="Login With Phone" />
       </View>
     );
   }
