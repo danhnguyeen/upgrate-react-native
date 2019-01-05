713a4c2ea765bc1c61efe68b6031976b77a46fef
Danh Nguyen <danhnguyen@Danhs-MacBook-Pro.local>
2019-01-05 17:19:27 +0700

    [Splash Screen] linked splash screen on iOS

--- App.js
+++ App.js
@@ -8,10 +8,14 @@
 
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
 
 import AppContainer from './navigators';
 
 export default class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
   render() {
     return (
       <AppContainer />
