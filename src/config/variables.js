0592a418ad6d346d3244dacbb0f71e2bb322100d
Danh Nguyen <danhnguyen@Danhs-MacBook-Pro.local>
2019-01-06 15:37:06 +0700

    [UI] added box shadow component

---
+++ src/config/variables.js
@@ -0,0 +1,93 @@
+import { Platform, Dimensions, PixelRatio, StatusBar } from 'react-native';
+import { Header } from 'react-navigation';
+
+export const DEVICE_HEIGTH = Dimensions.get('window').height;
+export const DEVICE_WIDTH = Dimensions.get('window').width;
+export const platform = Platform.OS;
+// export const isIphoneX =
+//   platform === 'ios' && DEVICE_HEIGTH === 812 && DEVICE_WIDTH === 375;
+
+const dimen = Dimensions.get('window');
+export const isIphoneX = Platform.OS === 'ios' &&
+      !Platform.isPad &&
+      !Platform.isTVOS &&
+      ((dimen.height === 812 || dimen.width === 812) || (dimen.height === 896 || dimen.width === 896));
+export function getStatusBarHeight(skipAndroid = false) {
+  if (Platform.OS === 'ios') {
+      return isIphoneX ? 44 : 20;
+  }
+  if (skipAndroid) {
+      return 0;
+  }
+  return StatusBar.currentHeight;
+};
+export const navHeight = Header.HEIGHT + getStatusBarHeight() - 19;
+// Colors
+export const brandPrimary = '#2997d8';
+export const brandInfo = '#5F9EE7';
+export const brandSuccess = '#7ED321';
+export const brandDanger = '#D0021B';
+export const brandWarning = "#f7941e";
+export const backgroundColor = "#efeef4";
+export const brandLight = "#212B34";
+export const brandLightOpacity50 = 'rgba(33, 43, 52, 0.5)';
+export const brandLightOpacity70 = 'rgba(28, 29, 32, 0.7)';
+export const lightBackground = '#fafbfc';
+export const statusBarColor = '#171e25';
+// Background Color
+
+// Shadow Color
+export const shadowColor = '#161616';
+// Font
+export const fontFamily = 'Roboto-Regular';
+export const fontFamilyBold = 'Roboto-Bold';
+
+let fontScal = 14;
+if (PixelRatio.get() > 2) {
+  fontScal = 15;
+}
+// if (PixelRatio.get() > 3) {
+//   fontScal = 18;
+// }
+
+export const fontSize = fontScal;
+export const inputFontSize = fontSize + 2;
+export const lineHeight = fontSize * 1.44;
+// export const fontSize = platform === 'ios' ? 12 : 14;
+export const fontSizeH1 = fontSize * 2.8;
+export const fontSizeH2 = fontSize * 2.4;
+export const fontSizeH3 = fontSize * 2;
+export const fontSizeH4 = fontSize * 1.5;
+// Text
+export const textColor = "#fff";
+export const textDarkColor = "#686868";
+export const inverseTextColor = "#000";
+export const textH1 = { fontSize: fontSizeH1, fontFamily: fontFamilyBold };
+export const textH2 = { fontSize: fontSizeH2, fontFamily: fontFamilyBold };
+export const textH3 = { fontSize: fontSizeH3, fontFamily: fontFamilyBold };
+export const textH4 = { fontSize: fontSizeH4, fontFamily: fontFamilyBold };
+
+// Title
+export const titleFontSize = fontSize + 1;
+
+export const shadowProperties = {
+  shadowColor: '#1a1917',
+  shadowOpacity: 0.25,
+  shadowOffset: { width: 0, height: 10 },
+  shadowRadius: 10
+}
+export const toastOption = {
+  duration: 4000,
+  position: -49,
+  shadow: false,
+  animation: true,
+  hideOnPress: true,
+  // delay: 1000,
+  containerStyle: {
+    // paddingVertical: 1,
+    paddingTop: 4,
+    paddingBottom: 6,
+    width: DEVICE_WIDTH,
+    borderRadius: 0
+  }
+};
--- src/containers/home/home.js
+++ src/containers/home/home.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, ScrollView } from 'react-native';
 import RNAccountKit from 'react-native-facebook-account-kit'
 import FCM from "react-native-fcm";
+
+import { backgroundColor } from '../../config/variables';
 
 class Home extends Component {
   componentDidMount() {
@@ -37,13 +39,36 @@ class Home extends Component {
   }
   render() {
     return (
-      <View>
-        <Text>Homefwe</Text>
-        <Button onPress={this.loginWithPhone} title="Login With Phone" />
-        <Button onPress={this.localPushNotification} title="Local Push" />
+      <View style={{ flex: 1, backgroundColor }}>
+        <ScrollView contentContainerStyle={{ paddingVertical: 15,  }}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={[styles.container, { marginLeft: 15, marginRight: 0, width: 250 }]}></View>
+            <View style={[styles.container, { marginLeft: 10, width: 250 }]}></View>
+          </ScrollView>
+          <View style={styles.container}></View>
+          {/* <View style={styles.container}></View> */}
+          <Text>Homefwe</Text>
+          <Button onPress={this.loginWithPhone} title="Login With Phone" />
+          <Button onPress={this.localPushNotification} title="Local Push" />
+        </ScrollView>
       </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 15,
+    backgroundColor: '#fff',
+    // width: '100%',
+    height: 180,
+    borderRadius: 5,
+    elevation: 2,
+    shadowColor: '#1a1917',
+    shadowOpacity: 0.1,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 5,
+    marginBottom: 15
+  }
+})
 export default Home;
