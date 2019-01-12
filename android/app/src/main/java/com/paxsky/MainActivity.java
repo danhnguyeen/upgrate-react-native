32019275e8d876d4009f8ca42150fe6f6f9ff864
Danh Nguyen <danhnguyen@Danhs-MacBook-Pro.local>
2019-01-12 21:23:35 +0700

    [Facebook login] added on Android

--- android/app/src/main/java/com/paxsky/MainActivity.java
+++ android/app/src/main/java/com/paxsky/MainActivity.java
@@ -11,6 +11,11 @@ public class MainActivity extends ReactActivity {
         SplashScreen.show(this);
         super.onCreate(savedInstanceState);
     }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        MainApplication.getCallbackManager().onActivityResult(requestCode, resultCode, data);
+    }
     /**
      * Returns the name of the main component registered from JavaScript.
      * This is used to schedule rendering of the component.
